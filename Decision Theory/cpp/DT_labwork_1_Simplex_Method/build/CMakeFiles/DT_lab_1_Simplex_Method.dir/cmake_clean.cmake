621d3c4cd1040bc5b489934fe9e219ef09a8261c
glofst <forglofst@gmail.com>
2020-05-30 20:50:14 +0300

    Update

---
+++ Decision Theory/cpp/DT_labwork_1_Simplex_Method/build/CMakeFiles/DT_lab_1_Simplex_Method.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DT_lab_1_Simplex_Method.dir/main.cpp.o"
+  "DT_lab_1_Simplex_Method"
+  "DT_lab_1_Simplex_Method.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DT_lab_1_Simplex_Method.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
